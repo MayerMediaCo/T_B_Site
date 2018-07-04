64f07af685d3b6166fe910266c7623f9ea6d9f45
danmayer4l <dannymayer@Dannys-MacBook-Pro.local>
2018-07-03 22:41:29 -0500

    Minor Footer changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,11 +7,19 @@ const Footer = () => {
       <div className='container'>
         <div className='columns'>
           <div className='column is-4'>
+            <h2 className='is-size-5'>Location</h2>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Theresa Buckley
             </p>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              1200 South York Road Suite 4240
+            </p>
+            <p>Elmhurst, IL 60126
+            </p>
+            <p>Phone:
+              <a href='tel:+1-630-914-4305'>
+                630-914-4305
+              </a>
             </p>
           </div>
           <div className='column is-4'>
@@ -22,9 +30,6 @@ const Footer = () => {
             </div>
           </div>
           <div className='column is-4'>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </p>
